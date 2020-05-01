a746e9e60da6eaee2ffda4110d0d923ebf248a5d
aaanestad <54600286+aaanestad@users.noreply.github.com>
2020-05-01 19:18:02 -0400

    Boom Almost Backend Working

--- pythonportfolio/calculations.py
+++ pythonportfolio/calculations.py
@@ -1,34 +1,40 @@
 def calculate(args):
     c = None
     for i in range(len(args)):
-        if args[i][1] == "":
+        if args[i][0] == "":
             break
-        elif args[i][1] == "+":
-            c = float(args[i][2]) + float(args[i+1][3])
-        elif args[i][1] == "-":
-            c = float(args[i][2]) - float(args[i+1][3])
-        elif args[i][1] == "*":
-            c = float(args[i][2]) * float(args[i+1][3])
-        elif args[i][1] == "/":
-            c = float(args[i][2]) / float(args[i+1][3])
+        elif args[i][0] == "+":
+            c = float(args[i][1]) + float(args[i][2])
+            args[i+1][1] = c
+        elif args[i][0] == "-":
+            c = float(args[i][1]) - float(args[i][2])
+            args[i+1][1] = c
+        elif args[i][0] == "*":
+            c = float(args[i][1]) * float(args[i][2])
+            args[i+1][1] = c
+        elif args[i][0] == "/":
+            c = float(args[i][1]) / float(args[i][2])
+            args[i+1][1] = c
     return c
 
 def parseArg(s):
-    args = [5*["", "", ""]]
+    args = [["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""]]
     numArgs = 0
-    dataType = "op"
+    firstNum = True
+    dataType = "num"
     for char in s:
-        if((char.isdigit() or char == ".") and numArgs == 0):
-            args[numArgs][2] = args[numArgs][2] + char
-        elif(char.isdigit() or char == "."):
-            dataType == "number"
-            args[numArgs][3] = args[numArgs][3] + char
-            args[numArgs + 1][2] = args[numArgs + 1][2] + char
-        elif(char in ["+", "-", "*", "/"]):
-            if dataType != "op":
-                numArgs += 1
-                dataType = "op"
+        if((char.isdigit() or char == ".") and firstNum == True):
             args[numArgs][1] = args[numArgs][1] + char
+        elif(char.isdigit() or char == "."):
+            args[numArgs][2] = args[numArgs][2] + char
+            args[numArgs + 1][1] = args[numArgs + 1][1] + char
+            if dataType != "num":
+                numArgs += 1
+                dataType = "num"
+        elif(char in ["+", "-", "*", "/"]):
+            firstNum = False;
+            args[numArgs][0] = args[numArgs][0] + char
+            dataType = "op"
         elif(char == " "):
             continue
         else:
