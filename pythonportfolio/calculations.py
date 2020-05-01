cb5d7d18dd8768bc46769a69bd39317587b2fc7f
aaanestad <54600286+aaanestad@users.noreply.github.com>
2020-05-01 19:43:36 -0400

    Ready Up Until OOP

--- pythonportfolio/calculations.py
+++ pythonportfolio/calculations.py
@@ -5,16 +5,16 @@ def calculate(args):
             break
         elif args[i][0] == "+":
             c = float(args[i][1]) + float(args[i][2])
-            args[i+1][1] = c
+            args[i+1][1] = float(c)
         elif args[i][0] == "-":
             c = float(args[i][1]) - float(args[i][2])
-            args[i+1][1] = c
+            args[i+1][1] = float(c)
         elif args[i][0] == "*":
             c = float(args[i][1]) * float(args[i][2])
-            args[i+1][1] = c
+            args[i+1][1] = float(c)
         elif args[i][0] == "/":
             c = float(args[i][1]) / float(args[i][2])
-            args[i+1][1] = c
+            args[i+1][1] = float(c)
     return c
 
 def parseArg(s):
@@ -26,15 +26,12 @@ def parseArg(s):
         if((char.isdigit() or char == ".") and firstNum == True):
             args[numArgs][1] = args[numArgs][1] + char
         elif(char.isdigit() or char == "."):
-            args[numArgs][2] = args[numArgs][2] + char
-            args[numArgs + 1][1] = args[numArgs + 1][1] + char
-            if dataType != "num":
-                numArgs += 1
-                dataType = "num"
+            args[numArgs-1][2] = args[numArgs-1][2] + char
+            args[numArgs][1] = args[numArgs][1] + char
         elif(char in ["+", "-", "*", "/"]):
             firstNum = False;
             args[numArgs][0] = args[numArgs][0] + char
-            dataType = "op"
+            numArgs += 1
         elif(char == " "):
             continue
         else:
@@ -44,11 +41,10 @@ def parseArg(s):
     return args
 
 def run():
-    command = input("What is your calculation?")
+    command = input("What is your calculation? ")
     args = parseArg(command)
     ans = calculate(args)
     print(ans)
     run()
-
 
 run()
