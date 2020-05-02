abc607494cf8cd7f6bcf20ff1197822a1c5be702
aaanestad <54600286+aaanestad@users.noreply.github.com>
2020-05-01 23:34:14 -0400

    Gui interface

--- pythonportfolio/picture.py
+++ pythonportfolio/picture.py
@@ -1,13 +1,5 @@
 from tkinter import *
 
-class mainCom():
-    def __init__ (self):
-        calcTime = " "
-
-    def makeString(self, char):
-        calcTime = str(char) + calcTime
-        print("newchar = " + calcTime)
-
 def run():
     print("pussy")
     print(com)
@@ -73,38 +65,46 @@ def parseArg(s):
             break
     return args
 
-com = mainCom()
-gui = Tk()
-btn = Button(gui, text="Enter", command=lambda: run(), height=4, width=8)
-btn.grid(row = 4, column = 2)
+def makeString(line, char):
+    line = str(char) + str(line)
+    print("newchar = " + line)
+    return line
 
-oneBtn = Button(gui, text = "1", height=4, width=8, bg='orange', command=lambda: com.makeString("1"))
-twoBtn = Button(gui, text = "2", height=4, width=8, bg='orange', command=lambda: com.makeString("2"))
-threeBtn = Button(gui, text = "3", height=4, width=8, bg='orange', command=lambda: com.makeString("3"))
-fourBtn = Button(gui, text = "4", height=4, width=8, bg='orange', command=lambda: com.makeString("4"))
-fiveBtn = Button(gui, text = "5", height=4, width=8, bg='orange', command=lambda: com.makeString("5"))
-sixBtn = Button(gui, text = "6", height=4, width=8, bg='orange', command=lambda: com.makeString("6"))
-sevenBtn = Button(gui, text = "7", height=4, width=8, bg='orange', command=lambda: com.makeString("7"))
-eightBtn = Button(gui, text = "8", height=4, width=8, bg='orange', command=lambda: com.makeString("8"))
-nineBtn = Button(gui, text = "9", height=4, width=8, bg='orange', command=lambda: com.makeString("9"))
-oneBtn.grid(row = 1, column = 0)
-twoBtn.grid(row = 1, column = 1)
-threeBtn.grid(row = 1, column = 2)
-fourBtn.grid(row = 2, column = 0)
-fiveBtn.grid(row = 2, column = 1)
-sixBtn.grid(row = 2, column = 2)
-sevenBtn.grid(row = 3, column = 0)
-eightBtn.grid(row = 3, column = 1)
-nineBtn.grid(row = 3, column = 2)
+def buildScreen():
+    com = " "
+    gui = Tk()
+    btn = Button(gui, text="Enter", command=lambda: run(), height=4, width=8)
+    btn.grid(row = 4, column = 2)
 
-plusBtn = Button(gui, text = "+", height=4, width=8, bg='gray', command=lambda: com.makeString("+"))
-minusBtn = Button(gui, text = "-", height=4, width=8, bg='gray', command=lambda: com.makeString("-"))
-multBtn = Button(gui, text = "*", height=4, width=8, bg='gray', command=lambda: com.makeString("*"))
-divBtn = Button(gui, text = "/", height=4, width=8, bg='gray', command=lambda: com.makeString("/"))
-plusBtn.grid(row = 1, column = 3)
-minusBtn.grid(row = 2, column = 3)
-multBtn.grid(row = 3, column = 3)
-divBtn.grid(row = 4, column = 3)
+    oneBtn = Button(gui, text = "1", height=4, width=8, bg='orange', command=lambda: makeString(com, "1"))
+    twoBtn = Button(gui, text = "2", height=4, width=8, bg='orange', command=lambda: makeString(com, "2"))
+    threeBtn = Button(gui, text = "3", height=4, width=8, bg='orange', command=lambda: makeString(com, "3"))
+    fourBtn = Button(gui, text = "4", height=4, width=8, bg='orange', command=lambda: makeString(com, "4"))
+    fiveBtn = Button(gui, text = "5", height=4, width=8, bg='orange', command=lambda: makeString(com, "5"))
+    sixBtn = Button(gui, text = "6", height=4, width=8, bg='orange', command=lambda: makeString(com, "6"))
+    sevenBtn = Button(gui, text = "7", height=4, width=8, bg='orange', command=lambda: makeString(com, "7"))
+    eightBtn = Button(gui, text = "8", height=4, width=8, bg='orange', command=lambda: makeString(com, "8"))
+    nineBtn = Button(gui, text = "9", height=4, width=8, bg='orange', command=lambda: makeString(com, "9"))
+    oneBtn.grid(row = 1, column = 0)
+    twoBtn.grid(row = 1, column = 1)
+    threeBtn.grid(row = 1, column = 2)
+    fourBtn.grid(row = 2, column = 0)
+    fiveBtn.grid(row = 2, column = 1)
+    sixBtn.grid(row = 2, column = 2)
+    sevenBtn.grid(row = 3, column = 0)
+    eightBtn.grid(row = 3, column = 1)
+    nineBtn.grid(row = 3, column = 2)
 
-gui.wm_geometry("500x500")
-gui.mainloop()
+    plusBtn = Button(gui, text = "+", height=4, width=8, bg='gray', command=lambda: makeString(com, "+"))
+    minusBtn = Button(gui, text = "-", height=4, width=8, bg='gray', command=lambda: makeString(com, "-"))
+    multBtn = Button(gui, text = "*", height=4, width=8, bg='gray', command=lambda: makeString(com, "*"))
+    divBtn = Button(gui, text = "/", height=4, width=8, bg='gray', command=lambda: makeString(com, "/"))
+    plusBtn.grid(row = 1, column = 3)
+    minusBtn.grid(row = 2, column = 3)
+    multBtn.grid(row = 3, column = 3)
+    divBtn.grid(row = 4, column = 3)
+
+    gui.wm_geometry("500x500")
+    gui.mainloop()
+
+buildScreen()
